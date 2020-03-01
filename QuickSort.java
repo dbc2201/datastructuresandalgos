aef4aa5a66ba546b4a84faca06dbdb7f150ac186
dbc2201 <divyanshb22@gmail.com>
2020-03-01 20:36:50 +0530

    replace for loop with enhanced for

--- QuickSort.java
+++ QuickSort.java
@@ -34,8 +34,8 @@ public class QuickSort {
     }
 
     public static void printArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+        for (int value : array) {
+            System.out.print(value + " ");
         }
     }
 }
