7edbf350486d319e1132a3c8f241d6dd309bde4d
dbc2201 <divyanshb22@gmail.com>
2020-03-01 20:38:40 +0530

    replace for loop with enhanced for

--- BinarySearch.java
+++ BinarySearch.java
@@ -22,8 +22,8 @@ public class BinarySearch {
 
     public static void printArray(int[] array) {
         System.out.println("---------------------------");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+        for (int value : array) {
+            System.out.print(value + " ");
         }
         System.out.println();
     }
